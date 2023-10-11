f914e09209a249336e20f5e945558d2836415882
Dheeraj Kurupati <117580661+Dheerajkurupati@users.noreply.github.com>
2023-10-11 12:31:00 +0530

    PROJECT.java

---
+++ PROJECT.java
@@ -0,0 +1,70 @@
+package k22ug;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.*;
+
+class ProcessEntity {
+    int processID;
+    String processName;
+    int executionTime;
+
+    public ProcessEntity(int processID, String processName, int executionTime) {
+        this.processID = processID;
+        this.processName = processName;
+        this.executionTime = executionTime;
+    }
+}
+
+class PriorityQueue {
+    LinkedList<ProcessEntity> queue = new LinkedList<>();
+
+    public void insert(ProcessEntity process) {
+        int index = 0;
+        while (index < queue.size() && process.executionTime > queue.get(index).executionTime) {
+            index++;
+        }
+        queue.add(index, process);
+    }
+
+    public ProcessEntity getShortestJob() {
+        if (!queue.isEmpty()) {
+            return queue.removeFirst();
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+}
+
+public class SJFScheduling {
+    public static void main(String[] args) {
+        PriorityQueue priorityQueue = new PriorityQueue();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of processes: ");
+        int numProcesses = scanner.nextInt();
+
+        for (int i = 0; i < numProcesses; i++) {
+//            System.out.print("Enter Process ID for Process " + (i + 1) + ": ");
+            int processID = new Random().nextInt(1000);
+            System.out.print("Enter Process Name for Process " + (i + 1) + ": ");
+            String processName = scanner.next();
+            System.out.print("Enter Execution Time for Process " + (i + 1) + ": ");
+            int executionTime = scanner.nextInt();
+
+            ProcessEntity process = new ProcessEntity(processID, processName, executionTime);
+            priorityQueue.insert(process);
+        }
+
+        System.out.println("\nProcess Scheduling Order (Shortest Job First):");
+        while (!priorityQueue.isEmpty()) {
+            ProcessEntity shortestJob = priorityQueue.getShortestJob();
+            System.out.println("Process ID: " + shortestJob.processID +
+                    ", Process Name: " + shortestJob.processName +
+                    ", Execution Time: " + shortestJob.executionTime);
+        }
+    }
+}
